97da9c49f71c942848b137ae4aa3054b7ba4ad3c
Anna Lima <annavanwesep@gmail.com>
2023-08-02 15:04:46 -0700

    added route to get one user

--- app/routes/user.py
+++ app/routes/user.py
@@ -9,6 +9,12 @@ from app.models.user import User
 # All routes defined with user_bp start with url_prefix (/users)
 user_bp = Blueprint("users", __name__, url_prefix="/users")
 
+#get single user
+@user_bp.route("/<id>", methods=['GET'])
+def user(id):
+    board = get_valid_item_by_id(User, id)
+    return board.to_dict(), 200
+
 @user_bp.route("", methods=['GET']) #Mark suggested using user here instead of id
 def get():
     #get token from the request header
