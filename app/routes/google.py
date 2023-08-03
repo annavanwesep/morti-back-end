70a5b2dc21a18f8015dbfc16060c671a9b22f9fd
Anna Lima <annavanwesep@gmail.com>
2023-08-03 09:13:20 -0700

    added token verification

--- app/routes/google.py
+++ app/routes/google.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, request, jsonify
+from flask import Flask, Blueprint, request, jsonify, session, abort
 from google_auth_oauthlib.flow import Flow
 # from .oauth_utils import get_google_user_info
 from google.oauth2 import id_token
 import google.auth.transport.requests
+from pip._vendor import cachecontrol
+from requests_oauthlib import OAuth2Session
+# from sqlalchemy import create_engine
+# from sqlalchemy import sessionmaker
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
 
 
 google_bp = Blueprint("google", __name__)
 
-@google_bp.route("/session", methods=["POST"])
+@google_bp.route("/google_callback", methods=["POST"])
 def session():
     print("HELLO")
     token = request.json.get("token")
@@ -18,20 +24,7 @@ def session():
         
     #Simulate token verification (implement actual)
     try: 
-        id_info = id_token.verify_oauth2_token(
-            id_token=token,
-            request=google.auth.transport.requests.Request(),
-            audience=["GOOGLE_CLIENT_ID"]
-        )    
-        print(id_info)
-    except ValueError:
-        return jsonify({"success": False, "error": "Invalid token"}), 400
         
-        flow.fetch_token(authorization_response=request.url)
-        
-        if not session["state"] == request.args["state"]:
-            abort(500) #State does not match!
-
         credentials = flow.credentials
         request_session = requests.session()
         cached_session = cachecontrol.CacheControl(request_session)
@@ -55,6 +48,62 @@ def session():
         last_name =id_info.get("family_name")
         email = id_info.get("email")
         
-        fake_database.save_user(google_id, first_name, last_name, email)
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO users (google_id, first_name, last_name, email) "
+                "VALUES (%s, %s, %s, %s)",
+                (google_id, first_name, last_name, email)
+            )
+            conn.commit()        
+        client_id = ["GOOGLE_CLIENT_ID"]
+        client_secret = ["GOOGLE_CLIENT_SECRET"]
+        redirect_uri = ["RENDER_DATABASE_URI"]  # The URI to which the user will be redirected after granting permission
         
-        return redirect("/dashboard")
+        
+        flow.fetch_token(authorization_response=request.url)
+
+        id_info = id_token.verify_oauth2_token(
+            id_token=token,
+            request=google.auth.transport.requests.Request(),
+            audience=["GOOGLE_CLIENT_ID"]
+        )    
+        print(id_info)
+        
+        if not session["state"] == request.args["state"]:
+            abort(500) #State does not match!
+    except ValueError:
+        return jsonify({"success": False, "error": "Invalid token"}), 400
+        
+        # flow.fetch_token(authorization_response=request.url)
+        
+        # if not session["state"] == request.args["state"]:
+        #     abort(500) #State does not match!
+
+        # credentials = flow.credentials
+        # request_session = requests.session()
+        # cached_session = cachecontrol.CacheControl(request_session)
+        # token_request = google.auth.transport.requests.Request(session=cached_session)
+
+        # id_info = id_token.verify_oauth2_token(
+        #     id_token=credentials._id_token,
+        #     request=token_request,
+        #     audience=["GOOGLE_CLIENT_ID"]
+        # )
+        
+        # #Save user information to the session
+        
+        # session["google_id"] = id_info.get("sub")
+        # session["name"] = id_info.get("name")
+        
+        # #Save user information to the database
+        
+        # google_id = id_info.get("sub")
+        # first_name = id_info.get("given_name")
+        # last_name =id_info.get("family_name")
+        # email = id_info.get("email")
+        
+        # fake_database.save_user(google_id, first_name, last_name, email)
+        
+        # return redirect("/dashboard")
+        
+@
