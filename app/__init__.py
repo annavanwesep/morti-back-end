6657abb807cfe2ef7012b3b88e719b8b11aed27c
Anna Lima <annavanwesep@gmail.com>
2023-08-10 09:27:24 -0700

    updated migrations folder for local schema changes

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,6 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("RENDER_DATABASE_URI")
-        print(app.config["SQLALCHEMY_DATABASE_URI"])
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
