01b72dc205a7be68fb4c0431f4458255487864ff
Ana Silvera <anasilverx@gmail.com>
2023-08-10 12:22:23 -0700

    implement GET route to retrieve all trusted users for current user

--- app/routes/trust_routes.py
+++ app/routes/trust_routes.py
@@ -30,3 +30,25 @@ def add_trusted_user():
     current_user.trusted_users.append(trusted_user)
     db.session.commit()
     return jsonify({'message': f'You now trust {trusted_user.first_name}'}), 201
+
+#Get all the people the current user trusts 
+@trust_bp.route("", methods=["GET"])
+@jwt_required()
+def get_all_trusted_people():
+    current_user_email = get_jwt_identity()
+    try:
+        current_user = User.query.filter_by(email=current_user_email).first()
+    except Exception as e:
+        print("ERROR", str(e))
+        return {"error": "An error ocurred when retriveing current user"}, 404
+    
+    trusted_people = current_user.trusted_users.all()
+
+    trusted_people_response = []
+    for person in trusted_people :
+        trusted_people_response.append(
+            {"first_name": person.first_name,
+            "last_name": person.last_name,
+            "email": person.email}
+        )
+    return jsonify(trusted_people_response), 200
